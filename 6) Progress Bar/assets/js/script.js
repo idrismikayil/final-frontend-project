99accbe0b085a8bc99e422d2181a2825b53a1636
idrismikayil <idrisrm@code.edu.az>
2021-12-25 01:07:53 +0400

    finish

---
+++ 6) Progress Bar/assets/js/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    $('#bar1').barfiller();
+});
+$(document).ready(function () {
+    $('#bar2').barfiller();
+});
+$(document).ready(function () {
+    $('#bar3').barfiller();
+});
+$(document).ready(function () {
+    $('#bar4').barfiller();
+});
+$(document).ready(function () {
+    $('#bar5').barfiller();
+});
+$(document).ready(function () {
+    $('#bar6').barfiller();
+});
+$(document).ready(function () {
+    $('#bar7').barfiller();
+});
+$(document).ready(function () {
+    $('#bar8').barfiller();
+});
+$(document).ready(function () {
+    $('#bar9').barfiller();
+});
